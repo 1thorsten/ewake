d2147545bcf6060e382807a615bece23627fa779
1thorsten <thorsten.winkler@online.de>
2022-01-19 10:11:29 +0100

    remove comments in tsconfig.json and preserve comments in webpack.prod.js

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -3,23 +3,24 @@
 // https://webpack.js.org/plugins/terser-webpack-plugin/
 // https://github.com/terser/terser
 // https://stackoverflow.com/questions/54660674/more-extensive-mangling-terser-webpack-plugin
-const TerserPlugin = require('terser-webpack-plugin');
 const {merge} = require('webpack-merge');
 const common = require('./webpack.common.js');
-const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
     mode: 'production',
+    // https://webpack.js.org/configuration/devtool/
+    devtool: 'nosources-source-map',
     optimization: {
         nodeEnv: 'production',
         minimize: true,
         minimizer: [
-            new TerserPlugin({
+            new TerserPlugin(  {
                 include: common.output.filename,
                 extractComments: false,
                 terserOptions: {
-                    output: {
-                        comments: false
+                    format: {
+                        comments: true
                     },
                     ecma: undefined,
                     warnings: false,
